5896666e25bd946023114c576f3a19943b32e09e
BaekSeungYeon <111503729+Backsungyeon@users.noreply.github.com>
2024-09-30 00:26:29 +0900

    Add files via upload

---
+++ ForLoop.java
@@ -0,0 +1,14 @@
+package java2024;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		int sum=0;
+		
+		for(int i=1; i < 50; i+=3) {
+			sum = sum + i;
+		}
+		System.out.println(sum);
+	}
+
+}
