19d5a52065e1f7af8bb5464e456f35690e04c388
BaekSeungYeon <111503729+Backsungyeon@users.noreply.github.com>
2024-09-30 01:40:29 +0900

    Add files via upload

---
+++ Array_3.java
@@ -0,0 +1,34 @@
+package java2024;
+
+import java.util.Scanner;
+
+public class Array_3 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("양의 정수 10개 입력>>");
+        int[] array = new int[9];
+
+        for (int i = 0; i < 9; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        System.out.print("자리수의 합이 9인 것은... ");
+        
+        for (int number : array) {
+            int sum = 0;
+            int temp = number; 
+
+            while (temp > 0) {
+                sum += temp % 10;
+                temp /= 10; 
+            }
+
+            if (sum == 9) {
+                System.out.print(number + " ");
+            }
+        }
+
+        scanner.close(); // Scanner 자원 해제
+    }
+}
