019a414507990d0c6e99c035576dcff913614df2
BaekSeungYeon <111503729+Backsungyeon@users.noreply.github.com>
2024-10-19 23:28:39 +0900

    Add files via upload

---
+++ 1019/report/IPTV.java
@@ -0,0 +1,32 @@
+package java2024;
+
+class TV3{
+	private int size;
+	public TV3(int size) {this.size = size;}
+	protected int getSize() {return size;}
+}
+class ColorTV2 extends TV3{
+	private int color;
+	public ColorTV2(int size, int color){
+		super(size);
+		this.color = color;
+	}
+	public void printProperty() {
+		System.out.println(super.getSize() + "인치 " + color + "컬러");
+	}
+}
+public class IPTV extends ColorTV2{
+	private String address;
+	public IPTV(String address, int size, int color) {
+		super(size, color);
+		this.address = address;
+	}
+	public void printProperty() {
+		System.out.print("나의 SmartTV는 " + address + " 주소의 ");
+		super.printProperty();
+	}
+	public static void main(String[] args) {
+		IPTV iptv = new IPTV("192.168.0.5", 77, 20000000);
+		iptv.printProperty();
+	}
+}
